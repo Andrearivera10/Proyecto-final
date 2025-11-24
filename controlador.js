08a34abf69f1978c672ac4b6784e6a33b3bae51b
desarrollo <desarrollo@academicos.cesde.edu.co>
2025-11-24 15:08:03 -0500

    se hace cambio

---
+++ controlador.js
@@ -0,0 +1,19 @@
+
+let boton=document.getElementById("boton")
+boton.addEventListener("click",function(evento){
+    evento.preventDefault()
+
+    let cajausuario=document.getElementById("cajausuario")
+    let cajacorreo=document.getElementById("cajacorreo")
+    let cajaciudad=document.getElementById("cajaciudad")
+    let cajatelefono=document.getElementById("cajatelefono")
+    let cajacontraseña=document.getElementById("cajacontraseña")
+
+    let datosDelFormulario={
+        usuario:cajaUsuario.value,
+        correo:cajaCorreo.value,
+        ciudad:cajaCiudad.value,
+        telefono:cajaTelefono.value,
+        contraseña:cajaContraseña.value,
+    }
+})
